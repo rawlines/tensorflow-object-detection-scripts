ab52ea24211ffa3a5101183f338631d650da1246
Gonzalo Pérez Vizuete <defaperacm@gmail.com>
2022-06-18 21:35:07 +0200

    Add svo_record in cpp

---
+++ svo_recording/build/CMakeFiles/SVO_Recording.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SVO_Recording.dir/main.o"
+  "SVO_Recording.pdb"
+  "SVO_Recording"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SVO_Recording.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
