c96bdd5fc166d27a0da9511a66b0e1f1ab77e0a2
Gonzalo Pérez Vizuete <defaperacm@gmail.com>
2022-06-19 19:03:11 +0200

    Optimize balance algorithm

--- preprocessing/partition_dataset.py
+++ preprocessing/partition_dataset.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from multiprocessing.dummy import current_process
 import os
 import re
 import argparse
@@ -9,9 +8,13 @@ import random
 
 import os.path as path
 import xml.etree.ElementTree as ET
+import utils.genetic_balanced as gen
 
+from functools import cmp_to_key
+from numpy import diff
+from tokenize import group
 from shutil import copyfile
-
+from cv2 import sort
 from scipy import rand
 
 
@@ -54,6 +57,13 @@ parser.add_argument(
     type=int,
     default=-1,
     required=False)
+
+parser.add_argument(
+    '--balance_with_genetic',
+    help='Will use a genetic algorithm to balance the dataset, CAUTION, WILL REQUIRE A POWERFUL COMPUTER',
+    default=False,
+    required=False,
+    action='store_true')
 
 
 def analyze_xml(xml_file):
@@ -106,7 +116,10 @@ def partition_dir(annotation_source, image_source, dest, train_ratio, **kwargs):
     random.shuffle(all_images)
 
     if kwargs["balanced"]:
-        copy_balanced(all_images, train_ratio, image_source, annotation_source, test_dir, train_dir, **kwargs)
+        if kwargs["genetic_balance"]:
+            copy_balanced_genetic(all_images, train_ratio, image_source, annotation_source, test_dir, train_dir, **kwargs)
+        else:
+            copy_balanced(all_images, train_ratio, image_source, annotation_source, test_dir, train_dir, **kwargs)
     else:
         num_images = len(all_images)
         train_images = math.ceil(num_images * train_ratio)
@@ -114,17 +127,12 @@ def partition_dir(annotation_source, image_source, dest, train_ratio, **kwargs):
         #train
         print(f"Processing images for training: {train_dir}")
         for img in all_images[:train_images]:
-            print(img)
             copy(img, image_source, annotation_source, train_dir)
 
         #test
         print(f"Processing images for test: {test_dir}")
         for img in all_images[train_images:]:
-            print(img)
             copy(img, image_source, annotation_source, test_dir)
-
-
-    #TODO: asegurar el balaceo de los datos
 
 def copy_balanced(all_images, train_ratio, image_source, annotation_source, test_dir, train_dir, **kwargs):
     print("Copyng dataset in balanced mode")
@@ -134,49 +142,134 @@ def copy_balanced(all_images, train_ratio, image_source, annotation_source, test
     class_individual_stats = []
     
     #Analyze all the dataset
-    for a in all_annotations:
+    for i, a in enumerate(all_annotations):
         result = analyze_xml(a)
-        class_individual_stats.append(result)
+        class_individual_stats.append([all_images[i], result])
         for k, v in result.items():
             if k in class_dict_stats:
                 class_dict_stats[k] += v
             else:
                 class_dict_stats[k] = v
     
-    print(class_dict_stats)
-    num_images = len(all_images)
-    train_images = math.ceil(num_images * train_ratio)
-    min_b_samples = kwargs["min_balanced_samples"] 
-    min_samples = min_b_samples if min_b_samples != -1 else min(result.values())
-    print(f"Min samples for balanced is {min_samples}")
-    # if min_samples < train_images:
-    #     train_images = min_b_samples if min_b_samples != -1 else min_samples
+    classes_sorted = list(map(lambda x: x[0], sorted(class_dict_stats.items(), key=lambda x: x[1])))
     
-    #Start copying balancing the dataset
-    class_dict_stats.clear()
-    for i, img in enumerate(all_images):
-        copy_to_train_votes = 0
-        copy_to_test_votes = 0
+    #sort classes from less items to more
+    def sort_func(a: list, b: list):
+        stats_a: dict = a[1]
+        stats_b: dict = b[1]
+        
+        num_keys_a = len(stats_a.keys())
+        num_keys_b = len(stats_b.keys())
+        
+        total_classes_a = sum(stats_a.values())
+        total_classes_b = sum(stats_b.values())
+        
+        difference_a = total_classes_a - num_keys_a
+        difference_b = total_classes_b - num_keys_b
+        
+        def fit(stats: dict):
+            _fit = 0
+            for k, v in stats.items():
+                _fit += classes_sorted.index(k) * v / class_dict_stats[k]
+            return _fit
+        
+        fit_a = fit(stats_a)
+        fit_b = fit(stats_b)
+        
+        fit_a += total_classes_a + difference_a + num_keys_a + total_classes_a
+        fit_b += total_classes_b + difference_b + num_keys_b + total_classes_b
+        return fit_a - fit_b
+
+        
+    sorted_list = sorted(class_individual_stats, key=cmp_to_key(sort_func))
+    
+    # Select the minimum number of samples
+    min_train_samples = 0
+    if kwargs["min_balanced_samples"] != -1:
+        min_train_samples = kwargs["min_balanced_samples"]
+    else:
+        min_train_samples = float('inf')
+        for v in class_dict_stats.values():
+            ## how many samples of each class we should take for train using the ratio
+            rationed_train_samples = math.floor(v * train_ratio)
+            if rationed_train_samples < min_train_samples:
+                min_train_samples = rationed_train_samples
+        
+    
+    print(f"Min samples for balanced train dataset is {min_train_samples}")
+    
+    #Start copying trying to balance the dataset
+    current_copy_stats = {}
+    for img, result in sorted_list:
+        must_copy = False
         
         #Check how many of each class are in each sample
-        class_stats = class_individual_stats[i]
+        class_stats: dict = result
         for k, v in class_stats.items():
-            current_classes = class_dict_stats[k] if k in class_dict_stats else v
-            if current_classes < min_samples:
-                copy_to_train_votes += 1
-            else:
-                copy_to_test_votes += 1
+            current_copies = current_copy_stats[k] if k in current_copy_stats else v
+            available_copies = class_dict_stats[k]
+            print(k, available_copies, current_copies)
+            if current_copies < min_train_samples and current_copies < available_copies:
+                must_copy = True
+                if k in current_copy_stats:
+                    current_copy_stats[k] += v
+                else:
+                    current_copy_stats[k] = v
                 
         #copy to train or test
-        if copy_to_train_votes > copy_to_test_votes:
+        if must_copy:
             copy(img, image_source, annotation_source, train_dir)
-            for k, v in class_stats.items():
-                if k in class_dict_stats:
-                    class_dict_stats[k] += v
-                else:
-                    class_dict_stats[k] = v
         else:
             copy(img, image_source, annotation_source, test_dir)
+    
+    
+def copy_balanced_genetic(all_images, train_ratio, image_source, annotation_source, test_dir, train_dir, **kwargs):
+    print("Copyng dataset in balanced mode using a gentic algorithm")
+    
+    all_annotations = [os.path.join(annotation_source, f'{img.split(".")[0]}.xml') for img in all_images]
+    class_dict_stats = {}
+    class_individual_stats = []
+    
+    #Analyze all the dataset
+    for i, a in enumerate(all_annotations):
+        result = analyze_xml(a)
+        class_individual_stats.append([all_images[i], result])
+        for k, v in result.items():
+            if k in class_dict_stats:
+                class_dict_stats[k] += v
+            else:
+                class_dict_stats[k] = v
+    
+    num_images = len(all_images)
+    train_images = math.ceil(num_images * train_ratio)
+    
+    genetic = gen.Genetic(class_individual_stats,
+                      train_images,
+                      iterations=1000,
+                      living_things=10,
+                      mutations=1,
+                      reproductions=2,
+                      n_threads=8,
+                      selection='tournament',
+                      probabilistic_repoblation=False,
+                      tournament_percent=0.3,
+                      probabilistic_mutation=True)
+
+    genetic.optimize()
+    print('Best score: ', genetic.best_score)
+    print('Best genotype: ', genetic.best_genotype)
+    
+    test_images = genetic.best_genotype.get_chain()
+    for img, result in test_images:
+        try:
+            all_images.remove(img)
+            copy(img, image_source, annotation_source, train_dir)
+        except Exception as e:
+            print(f"{img} already copied")
+        
+    for img in all_images:
+        copy(img, image_source, annotation_source, test_dir)
+        
 
 def main():
     args = parser.parse_args()
@@ -185,7 +278,8 @@ def main():
                   args.output_dir,
                   args.train_ratio,
                   balanced=args.balance_train,
-                  min_balanced_samples=args.min_balanced_samples)
+                  min_balanced_samples=args.min_balanced_samples,
+                  genetic_balance=args.balance_with_genetic)
 
 
 if __name__ == '__main__':
